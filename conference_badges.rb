530e548120c7c05f85e9e669c4366a967f442487
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-14 04:53:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array)
+  newArray = []
   array.each{ |name|
-    return "Hello, my name is #{name}."
+    newArray.push("Hello, my name is #{name}.")
   }
+  newArray
 end 
